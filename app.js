046db72bfde74e529417a167e2c57b5bf3044467
Islomjon95 <98774021+Islomjon95@users.noreply.github.com>
2024-02-05 21:57:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,41 +2,39 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = "6472532692:AAHVKbZFOTysnMb9kcY7fV8-PF3orQO_J0k";
 const {lotinga , kirillga} = require("./algoritm")
 const bot = new TelegramBot(token, {polling: true});
-
 let lotincha = false;
 let kirillcha = false;
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    const fullName = msg.chat.first_name
-    if(msg.text=="/start"){
-        bot.sendMessage(chatId, `Assalomu aleykum <i><b>${fullName}</b></i> ! Botimizga Xush kelibsiz! Botimiz haqida to'liq ma'lumot olish uchun /info ni bosing`, {
-            parse_mode: "HTML"
-        });
+    const fullName = msg.chat.first_name;
 
-    }else if(msg.text == "/info"){
-        bot.sendMessage(chatId, "Bu bot matnlarni kirildan lotin ga lotindan kirilga o'zgartirib beradi. Agar siz matnni lotincha ko'rinishini hohlasangiz /lotin ni ustiga bosing. Agar siz matnni kirilcha ko'rinishda xohlasangiz /kiril ni ustiga bosing")
+    switch (msg.text) {
+        case '/start':
+            bot.sendMessage(chatId, `Assalomu aleykum <i><b>${fullName}</b></i> ! Botimizga Xush kelibsiz! Botimiz haqida to'liq ma'lumot olish uchun /info ni bosing`, { parse_mode: 'HTML' });
+            break;
+        case '/info':
+            bot.sendMessage(chatId, "Bu bot matnlarni kirildan lotin ga lotindan kirilga o'zgartirib beradi. Agar siz matnni lotincha ko'rinishini hohlasangiz /lotin ni ustiga bosing. Agar siz matnni kirilcha ko'rinishda xohlasangiz /kiril ni ustiga bosing");
+            break;
+        case '/lotin':
+            kirillcha = false;
+            bot.sendMessage(chatId, "Siz kirilcha matnni yuborishingiz mumkin");
+            lotincha = true;
+            break;
+        case '/kiril':
+            lotincha = false;
+            bot.sendMessage(chatId, "Siz lotincha matnni yuborishingiz mumkin");
+            kirillcha = true;
+            break;
+        default:
+            if (lotincha) {
+                const krilltolotin = lotinga(msg.text);
+                bot.sendMessage(chatId, krilltolotin);
+            } else if (kirillcha) {
+                const lotintokirill = kirillga(msg.text);
+                bot.sendMessage(chatId, lotintokirill);
+            }
+            break;
     }
-    else if(msg.text == "/lotin"){
-        kirillcha = false;
-        bot.sendMessage(chatId, "Siz kirilcha matnni yuborishingiz mumkin")
-        lotincha = true;
-              
-    }
-    else if(msg.text=="/kiril"){
-        lotincha = false;
-        bot.sendMessage(chatId, "Siz lotincha matnni yuborishingiz mumkin")
-        kirillcha =true;
-            
-    }
-    else if(lotincha){
-        let matn = msg.text
-        let krilltolotin = lotinga(matn)
-        bot.sendMessage(chatId, krilltolotin)
-    } 
-    else if(kirillcha){
-        let matn = msg.text
-        let lotintokirill = kirillga(matn)
-        bot.sendMessage(chatId, lotintokirill)
-    }
-  });
+});
+
